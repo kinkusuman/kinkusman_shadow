ba35d9eb21281f72beb59d5ed4dacba9cfff3de7
machidamachio <>
2026-01-18 13:29:16 +0900

    1/18

--- Assets/XL.cs
+++ Assets/XL.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ButtonScript : MonoBehaviour
+public class XL : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -19,6 +19,6 @@ public class ButtonScript : MonoBehaviour
 
     public void OnClick()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Scenes");
     }
 }
